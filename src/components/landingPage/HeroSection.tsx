5e432b1a91426d5b879b2bd1f9682d7166505a07
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-01-25 19:28:32 +0300

    Make Here section responsive

--- src/components/landingPage/HeroSection.tsx
+++ src/components/landingPage/HeroSection.tsx
@@ -3,32 +3,33 @@ import Image from "next/image";
 import Navbar from "../layout/Navbar";
 // Icons
 import { GoArrowUpRight } from "react-icons/go";
+import Title from "../ui/Title";
 
 const HeroSection = () => {
   return (
     <div className="bg-mainGreen py-5 px-10 text-white">
       <Navbar />
 
-      <h1 className="mt-10">
+      <h1 className="mt-10 max-md:text-center">
         <div className="flex items-center gap-x-5">
-          <h2 className="text-6xl font-medium">Лучший способ управлять</h2>
-          <span className="p-2 bg-mainYellow rounded-full">
+          <Title text="Лучший способ управлять" />
+          <span className="p-2 bg-mainYellow rounded-full max-md:hidden">
             <GoArrowUpRight className="text-black w-10 h-10" />
           </span>
         </div>
-        <div className="flex gap-x-5 items-center mt-2">
+        <div className="flex gap-x-5 items-center mt-2 max-md:flex-col-reverse max-md:mt-0 gap-y-5 max-md:items-center">
           <p className="max-w-lg font-light">
             Уже более 200 человек используют наше приложение. Удобный контроль
             за собственными доходами и расходами, которые помогут сохранить
             деньги.
           </p>
-          <h2 className="text-6xl font-medium">своими финансами</h2>
+          <Title text="своими финансами" />
         </div>
       </h1>
 
-      <div className="-mt-32 relative">
+      <div className="-mt-32 relative max-md:-mt-20 max-sm:-mt-10 flex flex-col items-center gap-y-2">
         <Image width={1000} height={1000} src={"phones.svg"} alt="Phones" />
-        <button className="bg-white absolute bottom-24 left-0 py-2 px-8 rounded-full text-black flex items-center gap-x-2">
+        <button className="bg-white md:absolute w-fit bottom-24 max-lg:bottom-16 max-md:bottom-10 left-0 py-2 px-8 rounded-full text-black flex items-center gap-x-2">
           <Image
             width={25}
             height={25}
@@ -36,7 +37,7 @@ const HeroSection = () => {
             alt="Google Play"
             className="bg-transparent"
           />
-          <span>
+          <span className="max-sm:text-sm">
             <p>Скачать в</p>
             <p>Google Play</p>
           </span>
---
+++ src/components/ui/Title.tsx
@@ -0,0 +1,11 @@
+type TextProps = {
+  text: string;
+};
+
+const Title = ({ text }: TextProps) => {
+  return (
+    <h1 className="text-6xl font-medium max-lg:text-5xl max-md:text-4xl w-full">{text}</h1>
+  );
+};
+
+export default Title;
