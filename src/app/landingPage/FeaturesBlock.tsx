228096f3dfe4b35b4adb80141420efd9f5177265
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-01-29 18:35:15 +0300

    Make features-section responsive

--- src/app/landingPage/FeaturesBlock.tsx
+++ src/app/landingPage/FeaturesBlock.tsx
@@ -13,13 +13,13 @@ const FeaturesBlock = () => {
         />
       </div>
 
-      <div className="relative mt-10 flex flex-col items-center max-md:mt-5">
+      <div className="relative mt-10 flex flex-col items-center max-md:mt-5 2xl:mt-36">
         <Image
           width={400}
           height={400}
           src={"phone.svg"}
           alt="Phone"
-          className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 max-md:hidden"
+          className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 max-md:hidden 2xl:w-auto"
         />
         <Image
           width={400}
@@ -28,17 +28,21 @@ const FeaturesBlock = () => {
           alt="Phone"
           className="w-auto md:hidden"
         />
-        <div className="grid grid-cols-2 gap-x-72 gap-y-10 max-lg:gap-x-56 max-lg:gap-y-5 max-md:gap-x-16 max-sm:grid-cols-1">
+        <div className="grid grid-cols-2 gap-x-72 gap-y-10 max-lg:gap-x-56 max-lg:gap-y-5 max-md:gap-x-16 max-sm:grid-cols-1 xl:mt-6 2xl:gap-x-96">
           {featuresList.map((feature, index) => (
             <div
               key={index}
               className={`group flex cursor-pointer items-center gap-x-5 max-sm:flex-row-reverse max-sm:justify-self-start ${index % 2 !== 0 ? "flex-row-reverse justify-self-start" : "flex-row justify-self-end"}`}
             >
               <span>
-                <h5 className="text-lg font-medium">{feature.title}</h5>
-                <p className="text-sm font-light">{feature.paragraph}</p>
+                <h5 className="text-lg font-medium 2xl:text-2xl">
+                  {feature.title}
+                </h5>
+                <p className="text-sm font-light 2xl:text-lg">
+                  {feature.paragraph}
+                </p>
               </span>
-              <span className="h-12 w-12 shrink-0 rounded-full border border-black p-2.5 transition duration-300 group-hover:border-transparent group-hover:bg-mainYellow">
+              <span className="flex size-12 shrink-0 items-center justify-center rounded-full border border-black p-2.5 transition duration-300 group-hover:border-transparent group-hover:bg-mainYellow 2xl:size-20">
                 <Image
                   width={25}
                   height={25}
--- src/components/ui/SubTitle.tsx
+++ src/components/ui/SubTitle.tsx
@@ -7,8 +7,8 @@ type SubTitleProps = {
 const SubTitle = ({ title, paragraph }: SubTitleProps) => {
   return (
     <>
-      <h2 className="text-3xl font-medium max-sm:text-2xl">{title}</h2>
-      {paragraph ? <p className="font-light">{paragraph}</p> : ""}
+      <h2 className="text-3xl font-medium max-sm:text-2xl 2xl:text-7xl">{title}</h2>
+      {paragraph ? <p className="font-light 2xl:text-3xl">{paragraph}</p> : ""}
     </>
   );
 };
