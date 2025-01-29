e9310d60c077f4b97a69596ea4da85d3406c3375
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-01-29 17:22:39 +0300

    Fix the line appearing on the side of the screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="text-balance bg-gray-50">{children}</body>
+    <html lang="en" className="overflow-x-hidden">
+      <body className="text-balance overflow-x-hidden">{children}</body>
     </html>
   );
 }
