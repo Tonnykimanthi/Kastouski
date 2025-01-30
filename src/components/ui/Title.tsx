0826db73c49f37aa3467b0543f432365ea58aec5
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-01-30 15:24:57 +0300

    Make Title more responsive

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Title = () => {
   return (
-    <div className="mx-auto mt-10 w-fit max-w-[110rem] max-2xl:mt-20 max-sm:text-center 2xl:mt-32">
+    <div className="mx-auto mt-10 w-fit max-w-[150rem] max-2xl:mt-20 max-sm:text-center 2xl:mt-32">
       <div className="flex items-center gap-x-5">
-        <h2 className="w-full text-[68px] font-medium uppercase max-xl:text-5xl max-md:text-3xl 2xl:text-9xl">
+        <h2 className="whitespace-nowrap text-3xl md:text-5xl lg:text-6xl xl:text-[68px] 2xl:text-8xl">
           Лучший способ управлять
         </h2>
         <div className="max-sm:hidden">
@@ -13,16 +13,16 @@ const Title = () => {
             height={100}
             src={"/yellow-arrow-up-right.svg"}
             alt="Yellow arrow up right"
-            className="w-[102px] max-lg:w-20 max-md:w-16 2xl:w-64"
+            className="w-14 md:w-16 lg:w-20 2xl:w-[102px]"
           />
         </div>
       </div>
-      <div className="mt-2 flex items-center gap-x-5 gap-y-5 max-sm:flex-col-reverse">
+      <div className="mt-2 flex gap-x-5 gap-y-5 max-sm:flex-col-reverse">
         <p className="text-[18px] font-light xl:text-3xl 2xl:text-4xl">
           Уже более 200 человек используют наше приложение. Удобный контроль за
           собственными доходами и расходами, которые помогут сохранить деньги.
         </p>
-        <h2 className="w-fit text-[68px] font-medium uppercase max-xl:text-5xl max-md:text-3xl 2xl:text-9xl">
+        <h2 className="whitespace-nowrap text-3xl md:text-5xl lg:text-6xl xl:text-[68px] 2xl:text-8xl">
           своими финансами
         </h2>
       </div>
