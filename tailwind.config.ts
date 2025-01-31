9cc0c6fd776714ac5ad5f894a673483db9f13ce5
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-01-31 16:55:21 +0300

    Modify the mainGreen color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,15 @@ export default {
     extend: {
       colors: {
         mainYellow: "#FFE000",
-        mainGreen: "#38A765",
+        // mainGreen: "#38A765",
         lightMainGreen: "#5DBB83",
         lightGreen: "#C0F3CB",
         mintWhite: "#F2F9F3",
         mintGreen: "#E7FFD8",
+      },
+      backgroundImage: {
+        mainGreen:
+          "linear-gradient(to right, #41b36f, #41b36f 99%, #41b36f 96%, #41b36f 92%, #41b36f 85%, #41b36f 77%, #41b36f 67%, #41b36f 56%, #41b36f 44%, #41b36f 33%, #41b36f, 23%, #41b36f 15%, #41b36f 8%, #41b36f 4%, #41b36f 1%, #41b36f 0%)",
       },
     },
   },
